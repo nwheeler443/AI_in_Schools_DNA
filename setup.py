a8c0fcd387aadbb30a94c3d3b04e047695d1f499
Nicole Wheeler <nwheeler443@gmail.com>
2024-03-18 20:22:55 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@
 from distutils.core import setup
 
 setup(name='AI_In_Schools',
-      version='1.0',
-      description='AI in Schools Project',
-      author='Nicole Wheeler',
-      author_email='nwheeler443@gmail.com',
-      packages=['biopython'],
-      package_dir = {'': 'utils'}
-     )
+    version='1.0',
+    description='AI in Schools Project',
+    author='Nicole Wheeler',
+    author_email='nwheeler443@gmail.com',
+    packages=find_packages(where='utils'),  # This tells setuptools that your packages are under the 'utils' directory
+    install_requires=[
+        'biopython',
+    ]
+    )
