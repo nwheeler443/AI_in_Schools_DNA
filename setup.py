f7575920c84862f8a3a75280a3a1eb2375c2b2e2
Nicole Wheeler <nwheeler443@gmail.com>
2024-03-18 20:07:43 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='AI_In_Schools',
+      version='1.0',
+      description='AI in Schools Project',
+      author='Nicole Wheeler',
+      author_email='nwheeler443@gmail.com',
+      packages=['biopython'],
+     )
