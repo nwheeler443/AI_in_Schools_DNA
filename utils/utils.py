3bfd157d97e345b86742b7837dfd751f84f112fb
Nicole Wheeler <nwheeler443@gmail.com>
2024-05-17 17:23:05 +0100

    view_alignment

--- utils/utils.py
+++ utils/utils.py
@@ -64,7 +64,7 @@ def calculate_match_score(sequence, amino_acid_frequencies):
 clrs =  {'A':'#FA5F55','L':'#FA5F55','I':'#FA5F55','V':'#FA5F55','M':'#FA5F55','F':'#FA5F55','Y':'#FA5F55','W':'#FA5F55', # hydrophobic
                  'H':'#3395FF','K':'#3395FF','R':'#3395FF', # basic
                  'D':'#7DCEA0','E':'#7DCEA0', # acidic
-                 'S':'#EB984E','T':'#EB984E','N':'#EB984E','Q':'#EB984E', # polar
+                 'S':'#F5B041','T':'#F5B041','N':'#F5B041','Q':'#F5B041', # polar
                  'C':'#F5B7B1','U':'#F5B7B1','G':'#F5B7B1','P':'#F5B7B1', # special cases
                  '-':'white', '*':'black'}
 
